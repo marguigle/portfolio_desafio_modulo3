7d7f77d8d366611e9aeab9c7c6ff2ab163072841
Marcelo G Iglesia <marguiel@yahoo.com.ar>
2022-10-05 19:11:43 -0300

    se logra binding de fotomia

--- portfolio desafio modulo 3/portfolio-app/src/app/exp-laboral/exp-laboral.component.ts
+++ portfolio desafio modulo 3/portfolio-app/src/app/exp-laboral/exp-laboral.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from '../servicios/portfolio.service';
 
 @Component({
   selector: 'app-exp-laboral',
   templateUrl: './exp-laboral.component.html',
-  styleUrls: ['./exp-laboral.component.css']
+  styleUrls: ['./exp-laboral.component.css'],
 })
 export class ExpLaboralComponent implements OnInit {
-
-  constructor() { }
+  constructor(private datosPortfolio: PortfolioService) {}
+  miPortfolio: any;
 
   ngOnInit(): void {
+    /* this.datosPortfolio.obtenerDatos().subscribe((data) => {
+      console.log(data);
+      this.miPortfolio = data;*/
   }
-
 }
